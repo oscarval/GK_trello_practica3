a82f70d0fb130749615c3aba061c89ce749aed07
Alexis <entulinea.alexis@gmail.com>
2020-05-15 19:19:17 +0200

    Restructure files of project. Delete items no used

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>
+      <main>
+      </main>
+    </div>
+  );
+}
+
+export default App;
