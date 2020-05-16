b55d409f37c0e4def2e598ef5ab5b596bc01f47e
Alexis <entulinea.alexis@gmail.com>
2020-05-16 18:46:11 +0200

    Set new colors First layout of list and items

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,33 +6,33 @@ const Main = (props) => {
   return (
     <div className='Main'>
       <div className='Main-lists'>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
-        <div className='Main-list'> Lista 1</div>
+        <div className='Main-list'>
+          <div className='Main-list-title'>Title</div>
+          <div className='Main-list-items'>
+            <div
+              draggable='true'
+              className='item'
+              key='item-1111'
+              id='item-1111'>
+              <textarea className='item-textarea'></textarea>
+            </div>
+            <div className='item' key='item-2222' id='item-2222'>
+              <textarea className='item-textarea'></textarea>
+            </div>
+            <div className='item' key='item-3333' id='item-3333'>
+              <textarea className='item-textarea'></textarea>
+            </div>
+            <div className='item' key='item-4444' id='item-44444'>
+              <textarea className='item-textarea'></textarea>
+            </div>
+          </div>
+          <div className='Main-list-footer'>
+            <div className='Main-list-add-item'>+Add Item</div>
+          </div>
+        </div>
+        <div className='Main-list'> Lista 2</div>
+        <div className='Main-list'> Lista 3</div>
+        <div className='Main-list'> Lista 4</div>
       </div>
     </div>
   );
